db55a5c4fe240a3d3967937f6d433eb175f32994
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-08 13:13:22 +0800

    Delete Rfc_Errata directory

--- Rfc_Errata/__init__.py
+++
@@ -1,4 +0,0 @@
-# Static values
-__version__  = '0.3.0'
-NAME         = 'rfc-errata'
-VERSION      = [ int(i) if i.isdigit() else i for i in __version__.split('.') ]
--- Rfc_Errata/css/errata.js
+++
@@ -1,4 +0,0 @@
-function hideFunction(nodeId) {
-   var ul = document.getElementById(nodeId)
-   ul.className = (ul.className=="nodeOpenClass") ? "nodeCloseClass" : "nodeOpenClass"
-}
